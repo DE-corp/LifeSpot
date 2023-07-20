57aa6ead43bbc15bf11228df8c2349db0361cee7
DE-corp <crazyHOPHOP@rambler.ru>
2023-07-20 13:06:31 +0300

    Вынес проверку возраста и сохранение сессии в отдельную функцию

--- LifeSpot/Static/scripts/index.js
+++ LifeSpot/Static/scripts/index.js
@@ -1,28 +1,34 @@
-﻿// создадим объект Map для хранения сессии
-let session = new Map();
-// Сохраним UserAgent
-session.set("userAgent", window.navigator.userAgent)
+﻿/*
+* Функция для проверки и сохранения  данных пользователя
+* Также блокирует доступ к сайту лицам, не подтвердившим свой возраст
+*
+* */
 
+function handleSession() {
 
-// Запросим возраст пользователя и тоже сохраним
-session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+    // создадим объект Map для хранения сессии
+    let session = new Map();
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
 
-// Проверка на возраст и сохранение сессии
-if (session.get("age") >= 18) {
-    let startDate = new Date().toLocaleString();
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
 
-    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-    session.set("startDate", startDate)
-}
-else {
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
-    a = true + 20 + "name"
-}
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
 
-// Вывод в консоль
-for (let result of session) {
-    console.log(result)
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
 }
 
 
