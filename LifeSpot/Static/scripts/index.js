1b8b79450b67268b82af34c9d43a8aaeab23140e
DE-corp <crazyHOPHOP@rambler.ru>
2023-07-24 18:47:16 +0300

    Переделал функцию фильтрации контента так, чтобы она принимала как аргумент другую функцию (ту, которая вытаскивает данные, введённые пользователем).

--- LifeSpot/Static/scripts/index.js
+++ LifeSpot/Static/scripts/index.js
@@ -44,17 +44,16 @@ function handleSession() {
 * Будет вызываться благодаря атрибуту oninput на index.html
 *
 * */
-function filterContent(inputString) {
+function filterContent(inputParseFunction) { // в этот параметр будет передана ФУНКЦИЯ парсинга пользовательского ввода
 
     // Находим контейнеры с видео, которые необходимо фильтровать
     let elements = document.getElementsByClassName('video-container');
-
     // Пробегаемся по контейнерам
     for (let i = 0; i <= elements.length; i++) {
         // Вытаскиваем текст описания видео, которое необходимо отфильтровать
         let videoText = elements[i].querySelector(".video-title").innerText;
         // Выполняем фильтрацию, сравнивая значения в нижнем регистре
-        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+        if (!videoText.toLowerCase().includes(inputParseFunction())) {
             // Скрываем неподходящие
             elements[i].style.display = 'none';
         } else {
